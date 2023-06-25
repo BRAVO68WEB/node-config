e43c067ab1cf1db506de2ddf09d04a15c8b09d99
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2023-06-25 13:48:42 +0530

    release: v1.1.0 for eslint-config

--- apps/eslint-config/src/default.ts
+++ apps/eslint-config/src/default.ts
@@ -37,7 +37,7 @@ const config: Linter.Config = {
     rules: {
         quotes: [
             2,
-            "single",
+            "double",
             {
                 avoidEscape: true,
             },
